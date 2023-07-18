5a6cec873a65c4f1ea725f71b508449fdac1edb4
hemraj shaqawal <hemrajshaqawal007@gmail.com>
2023-07-18 18:03:38 +0530

    Update Constant.js

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,4 +1,4 @@
-const key = 'AIzaSyCkFDPWQjGO-lCT3nAMoBldLRPWi9Cobz0'
+const key = ''
 const mostPopularVideo = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=50&key=' + key
 const suggestionApi = 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
 const LIVECHATOFFSET = 50
